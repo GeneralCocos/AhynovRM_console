7273393d3a05f7f209f621e0c51f57ed420754d5
GeneralCocos <vl@d-sotskov.ru>
2026-05-28 22:05:34 +0300

    Add *.cs-files

---
+++ New_Class2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AhynovRM
+{
+    internal class New_Class2
+    {
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// Ахунов Руслан Маратович Ам-1-25
+internal class Program
+{
+    static public double Sqr(double X)
+    {
+        return X * X;
+    }
+
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine(Sqr(5));
+    }
+}
